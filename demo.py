58dccfbf949560f13ec5423c03f0058fd22a3cbe
Atharva Deshpande <atharva.a.deshpande@accenture.com>
2025-04-29 14:29:26 +0100

    demo file added

---
+++ demo.py
@@ -0,0 +1,52 @@
+
+      
+import os  
+import base64
+from openai import AzureOpenAI  
+
+endpoint = os.getenv("ENDPOINT_URL", "https://datastudioopenai.openai.azure.com/openai/deployments/test/chat/completions?api-version=2025-01-01-preview")  
+deployment = os.getenv("DEPLOYMENT_NAME", "test")  
+subscription_key = os.getenv("AZURE_OPENAI_API_KEY", "97ff1dd8a91c44aa94af45a603808edd")  
+
+# Initialize Azure OpenAI Service client with key-based authentication    
+client = AzureOpenAI(  
+    azure_endpoint=endpoint,  
+    api_key=subscription_key,  
+    api_version="2025-01-01-preview",
+)
+    
+    
+# IMAGE_PATH = "YOUR_IMAGE_PATH"
+# encoded_image = base64.b64encode(open(IMAGE_PATH, 'rb').read()).decode('ascii')
+
+#Prepare the chat prompt 
+chat_prompt = [
+    {
+        "role": "system",
+        "content": [
+            {
+                "type": "text",
+                "text": "You are an AI assistant that helps people find information."
+            }
+        ]
+    }
+] 
+    
+# Include speech result if speech is enabled  
+messages = chat_prompt  
+    
+# Generate the completion  
+completion = client.chat.completions.create(  
+    model=deployment,
+    messages=messages,
+    max_tokens=800,  
+    temperature=0.7,  
+    top_p=0.95,  
+    frequency_penalty=0,  
+    presence_penalty=0,
+    stop=None,  
+    stream=False
+)
+
+print(completion.to_json())  
+    
